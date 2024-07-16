21efb2383afd86dad786e6bb3fa55a13bc18f8c6
evgenvandev <developjev@gmail.com>
2024-07-16 16:30:46 +0500

    01_commit_init_page-220

---
+++ Tennjs/script.js
@@ -0,0 +1,14 @@
+// Creating a canvas for displaying the game
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d");
+let width = canvas.width;
+let height = canvas.height;
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, width, height);
+
+// Draw the ball
+const BALL_SIZE = 5;
+let ballPosition = {x: 20, y: 30};
+
+ctx.fillStyle = "white";
+ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
