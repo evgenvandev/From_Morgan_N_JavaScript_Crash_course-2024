39880ce739a5d8d89f17823ad06ff46b0d4b783f
evgenvandev <developjev@gmail.com>
2024-07-17 16:31:31 +0500

    02_commit_add_simple_move_for_ball_page-224

--- Tennjs/script.js
+++ Tennjs/script.js
@@ -1,14 +1,37 @@
-// Creating a canvas for displaying the game
+// Get a canvas for displaying the game
 let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, width, height);
 
-// Draw the ball
+// The parameters of the ball
 const BALL_SIZE = 5;
 let ballPosition = {x: 20, y: 30};
+// The speed of the ball along the X axis and along the axis Y
+let xSpeed = 4;
+let ySpeed = 2;
 
-ctx.fillStyle = "white";
-ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
+function draw() {
+    // Filling canvas in black
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, width, height);
+
+    // Drawing the ball in white
+    ctx.fillStyle = "white";
+    ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
+}
+
+function update() {
+    ballPosition.x += xSpeed;
+    ballPosition.y += ySpeed;
+}
+
+function gameLoop() {
+    draw();
+    update();
+
+    // Call this function again after a timeout
+    setTimeout(gameLoop, 30);
+}
+
+gameLoop();
